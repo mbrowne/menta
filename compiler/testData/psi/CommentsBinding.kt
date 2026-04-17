331f4f9cd5112d9986a676f9e9ea893ba9bed942
Matt Browne <mbrowne@artnet.com>
2026-04-16 20:48:00 -0400

    Revert "chore: update license notice"
    This reverts commit 8db63ad5a2314b758b73e4a652eb423f7410dd74.

--- compiler/testData/psi/CommentsBinding.kt
+++ compiler/testData/psi/CommentsBinding.kt
@@ -1,7 +1,6 @@
 // COMPILATION_ERRORS
 
 /*
- * Note: This file may have been modified from its original version from Kotlin.
  * Copyright JetBrains s.r.o.
  */
 package foo.bar // package directive
